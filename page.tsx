f28a471477f16eb7e128d8a07ff62f6f8fe91df1
tolandreservices <tolandreservices@gmail.com>
2025-09-05 19:49:35 -0400

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'ui-sans-serif', padding: 24 }}>
+      <h1>Billazer Backend</h1>
+      <p>API is deployed. Try POSTing to <code>/api/compute</code>.</p>
+    </main>
+  );
+}
